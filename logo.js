afeec0e56984d11c04146433a91aaebe29264a16
rearden bentley <102398009+reardenbentleymakesmusic@users.noreply.github.com>
2022-03-26 14:46:08 -0400

    Update logo.js

--- logo.js
+++ logo.js
@@ -3,12 +3,14 @@ let ctx = canvas.getContext("2d");
 
 let scale = 50;
 
+let initial = true;
+
 function newRandomColor() {
   return '#' + Math.floor(Math.random()*16777216).toString(16);
 }
 
-function drawSquare(x, y, white) {
-  if(white == true) {
+function drawSquare(x, y) {
+  if(initial == true) {
     ctx.fillStyle = "#ffffff";
   } else {
     ctx.fillStyle = newRandomColor();
@@ -17,36 +19,35 @@ function drawSquare(x, y, white) {
 }
 
 class Square {
-  constructor(x, y, white) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.white = white;
-    drawSquare(this.x, this.y, this.white);
+    drawSquare(this.x, this.y);
   }
 }
 
 let squares = [
-  new Square(0, 1, true),
-  new Square(0, 2, true),
-  new Square(0, 3, true),
-  new Square(0, 4, true),
-  new Square(0, 5, true),
-  new Square(1, 1, true),
-  new Square(1, 3, true),
-  new Square(2, 1, true),
-  new Square(2, 2, true),
-  new Square(2, 4, true),
-  new Square(2, 5, true),
-  new Square(4, 1, true),
-  new Square(4, 2, true),
-  new Square(4, 3, true),
-  new Square(4, 4, true),
-  new Square(4, 5, true),
-  new Square(5, 1, true),
-  new Square(5, 3, true),
-  new Square(5, 5, true),
-  new Square(6, 2, true),
-  new Square(6, 4, true)
+  new Square(0, 1),
+  new Square(0, 2),
+  new Square(0, 3),
+  new Square(0, 4),
+  new Square(0, 5),
+  new Square(1, 1),
+  new Square(1, 3),
+  new Square(2, 1),
+  new Square(2, 2),
+  new Square(2, 4),
+  new Square(2, 5),
+  new Square(4, 1),
+  new Square(4, 2),
+  new Square(4, 3),
+  new Square(4, 4),
+  new Square(4, 5),
+  new Square(5, 1),
+  new Square(5, 3),
+  new Square(5, 5),
+  new Square(6, 2),
+  new Square(6, 4)
 ];
 
 for(square of squares) {
@@ -54,11 +55,12 @@ for(square of squares) {
 }
 
 canvas.addEventListener('mousedown', e => {
+  initial = false;
   let cursorX = Math.floor((e.offsetX)/scale);
   let cursorY = Math.floor((e.offsetY)/scale);
   for(square of squares) {
     if(cursorX == square.x && cursorY == square.y) {
-      squares[squares.indexOf(square)] = new Square(cursorX, cursorY, false)
+      squares[squares.indexOf(square)] = new Square(cursorX, cursorY)
     }
   }
 });
