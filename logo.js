669d3d4d7f78ae5007fd989ecb3fa844c567c68c
rearden bentley <102398009+reardenbentleymakesmusic@users.noreply.github.com>
2022-03-26 00:09:05 -0400

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,3 +1,32 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
+let scale = 60;
+
+drawSquare(0*scale, 1*scale);
+drawSquare(0*scale, 2*scale);
+drawSquare(0*scale, 3*scale);
+drawSquare(0*scale, 4*scale);
+drawSquare(0*scale, 5*scale);
+drawSquare(1*scale, 1*scale);
+drawSquare(1*scale, 3*scale);
+drawSquare(2*scale, 1*scale);
+drawSquare(2*scale, 2*scale);
+drawSquare(2*scale, 4*scale);
+drawSquare(2*scale, 5*scale);
+
+drawSquare(4*scale, 1*scale);
+drawSquare(4*scale, 2*scale);
+drawSquare(4*scale, 3*scale);
+drawSquare(4*scale, 4*scale);
+drawSquare(4*scale, 5*scale);
+drawSquare(5*scale, 1*scale);
+drawSquare(5*scale, 3*scale);
+drawSquare(5*scale, 5*scale);
+drawSquare(6*scale, 2*scale);
+drawSquare(6*scale, 4*scale);
+
+function drawSquare(x, y) {
+  ctx.fillStyle = #ffffff;
+  ctx.fillRect(x, y, scale, scale);
+}
