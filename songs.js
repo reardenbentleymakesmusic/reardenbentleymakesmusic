6ce5eed6acb43f93e4d877f798834527734d59e0
rearden bentley <102398009+reardenbentleymakesmusic@users.noreply.github.com>
2022-03-28 22:19:28 -0400

    Update songs.js

--- songs.js
+++ songs.js
@@ -5,10 +5,8 @@ $(document).ready(function(){
   
   $("[id^='count']").click(function(){
     let index = $(this).attr('id').substring(5);
-    console.log(index);
     let name = "songs" + index;
-    console.log(name);
-    $(name).toggle();
+    $('name').toggle();
   });
   
 });
